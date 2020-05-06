29f2286d90037a76a88a674d4ab12690dcafcbab
Ikafathiah <60720854+Ikafathiah@users.noreply.github.com>
2020-05-06 20:10:23 +0700

    Add files via upload

---
+++ minggu13/tugas3/Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+/**
+ *
+ * @author ikafa
+ */
+public class Mahasiswa<T> {
+     T nama, nilai;
+    Mahasiswa<T> next, prev;
+
+    public Mahasiswa(Mahasiswa<T> prev, T nama, T nilai, Mahasiswa<T> next) {
+        this.prev = prev;
+        this.nama = nama;
+        this.nilai = nilai;
+        this.next = next;
+    }
+}
+
+
